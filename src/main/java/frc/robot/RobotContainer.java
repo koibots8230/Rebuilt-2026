4c40cb8a770487af6f9cce05871d7106a95fb594
gar821 <joshuagurara@gmail.com>
2026-01-30 11:24:22 -0500

    created commands for climber

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,32 +1,36 @@
 package frc.robot;
 
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.NotLogged;
+import edu.wpi.first.wpilibj.PS5Controller;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj.PS5Controller;
+
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.Climber;
 
 
 @Logged
 public class RobotContainer {
+  @NotLogged private final PS5Controller controller;
+  private final Climber climber;
 
-  private final PS5Controller controller;
-  
   public RobotContainer() {
+    climber = new Climber();
     controller = new PS5Controller(0);
 
     configureBindings();
   }
 
   private void configureBindings() {
+    Trigger raiseClimber = new Trigger(() -> controller.getPOV() == 0);
+    raiseClimber.onTrue(climber.raiseClimbCommand());
     
+    Trigger lowerClimber = new Trigger(() -> controller.getPOV() == 180);
+    lowerClimber.onTrue(climber.lowerClimbCommand());
   }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
 }
-
-  /* todo make cases (commands)
-   * Up D-Pad raise.
-   * Down D-Pad lower.
-   */
