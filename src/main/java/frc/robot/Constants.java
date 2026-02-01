3a162805f6b4785d2dba5615fe28945856bb2c3a
Molly <himollyh4@gmail.com>
2026-02-01 16:36:20 -0500

    fixed spotless

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,4 +2,8 @@ package frc.robot;
 
 public class Constants {
   public class RobotConstants {}
+
+  public static class IntakeConstants {
+    public static final int MOTOR_ID = 10;
+  }
 }
--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -2,19 +2,26 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
 
 public class Indexer extends SubsystemBase {
   private final SparkMax motor;
 
   public Indexer() {
-    motor = new SparkMax(10, MotorType.kBrushless);
+    motor = new SparkMax(IntakeConstants.MOTOR_ID, MotorType.kBrushless);
+
+    // motor.resetToFactory() how do I make sure that the motor are in the state
+    // that we want
   }
 
   private void setSpeed(double percent) {
     motor.set(percent);
+
+    SmartDashboard.putString("INDEXER CURRENT SPEED", Double.toString(percent));
   }
 
   public Command setSpeedCommand(double percent) {
