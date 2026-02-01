e06be9d2b2767599abbe30c7eb33a1f7dab1ca76
Molly <himollyh4@gmail.com>
2026-02-01 16:31:54 -0500

    fixed spotless

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -1,25 +1,23 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-
+import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Indexer extends SubsystemBase {
-    private final SparkMax motor;
-    
-    public Indexer(){
-        motor = new SparkMax(10, MotorType.kBrushless);
+  private final SparkMax motor;
 
-    }
+  public Indexer() {
+    motor = new SparkMax(10, MotorType.kBrushless);
+  }
 
-    private void setSpeed(double percent){
-        motor.set(percent);
-    }
+  private void setSpeed(double percent) {
+    motor.set(percent);
+  }
 
-    public Command setSpeedCommand(double percent){
-        return Commands.runOnce(() -> this.setSpeed(percent), this);
-    }
+  public Command setSpeedCommand(double percent) {
+    return Commands.runOnce(() -> this.setSpeed(percent), this);
+  }
 }
