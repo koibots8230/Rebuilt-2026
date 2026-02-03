3032f4fe6fc515141f92d7e5f513daa5be0b97f5
Jackson Shartzer <jacksonshartzer@gmail.com>
2026-02-03 15:24:32 -0500

    Run spotless

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,14 +1,8 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.*;
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.RPM;
-
-import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Current;
-import frc.lib.util.FeedforwardGains;
-import frc.lib.util.PIDGains;
-
-import static edu.wpi.first.units.Units.*;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.measure.AngularAcceleration;
@@ -47,14 +41,14 @@ public class Constants {
   public static class RobotConstants {
     public static final Time CLOCK_SPEED = Milliseconds.of(20);
   }
-    public static class ShooterConstants {
 
-        public static final AngularVelocity SHOOT_SPEED = RPM.of(3000);
-        public static final PIDGains PID = new PIDGains.Builder().kp(0.0).build();
-        public static final FeedforwardGains FEEDFORWARD =
-            new FeedforwardGains.Builder().kv(0.0).build();
-        public static final int MOTOR_PORT = 0;
-        public static final Current CURRENT_LIMIT = Amps.of(80);
+  public static class ShooterConstants {
 
-    }
+    public static final AngularVelocity SHOOT_SPEED = RPM.of(3000);
+    public static final PIDGains PID = new PIDGains.Builder().kp(0.0).build();
+    public static final FeedforwardGains FEEDFORWARD =
+        new FeedforwardGains.Builder().kv(0.0).build();
+    public static final int MOTOR_PORT = 0;
+    public static final Current CURRENT_LIMIT = Amps.of(80);
+  }
 }
