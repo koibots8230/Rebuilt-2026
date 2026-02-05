178afe15aa08c133e46d72aaed961dd694c4f447
ConradSkelly <conradskelly@gmail.com>
2026-02-05 14:59:53 -0500

    fixed ID

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -63,12 +63,12 @@ public class Constants {
 
     public static final double ROTATION_SCALAR = 1;
 
-    public static final int FRONT_LEFT_DRIVE_ID = 3;
-    public static final int FRONT_LEFT_TURN_ID = 4;
-    public static final int FRONT_RIGHT_DRIVE_ID = 5;
-    public static final int FRONT_RIGHT_TURN_ID = 6;
-    public static final int BACK_LEFT_DRIVE_ID = 1;
-    public static final int BACK_LEFT_TURN_ID = 2;
+    public static final int FRONT_LEFT_DRIVE_ID = 1;
+    public static final int FRONT_LEFT_TURN_ID = 2;
+    public static final int FRONT_RIGHT_DRIVE_ID = 3;
+    public static final int FRONT_RIGHT_TURN_ID = 4;
+    public static final int BACK_LEFT_DRIVE_ID = 5;
+    public static final int BACK_LEFT_TURN_ID = 6;
     public static final int BACK_RIGHT_DRIVE_ID = 7;
     public static final int BACK_RIGHT_TURN_ID = 8;
 
@@ -80,7 +80,7 @@ public class Constants {
 
     public static final int MAX_MOTOR_CURRENT_AMPS = 60;
 
-    public static final int MOTOR_ID = 13;
+    public static final int MOTOR_ID = 21;
   }
 
   public static class IntakeConstants {
@@ -99,7 +99,7 @@ public class Constants {
     public static final AngularVelocity MAX_VELOCITY = DegreesPerSecond.of(90);
     public static final AngularAcceleration MAX_ACCELERATION = DegreesPerSecondPerSecond.of(90);
     public static final Current CURRENT_LIMIT = Amps.of(60);
-    public static final int MOTOR_ID = 12;
+    public static final int MOTOR_ID = 11;
   }
 
   public static class ShooterConstants {
@@ -108,7 +108,7 @@ public class Constants {
     public static final PIDGains PID = new PIDGains.Builder().kp(0.0).build();
     public static final FeedforwardGains FEEDFORWARD =
         new FeedforwardGains.Builder().kv(0.1).build();
-    public static final int MOTOR_PORT = 11;
+    public static final int MOTOR_PORT = 20;
     public static final Current CURRENT_LIMIT = Amps.of(60);
   }
 
@@ -138,7 +138,7 @@ public class Constants {
     public static final Distance WHEEL_DIAMETER =
         Distance.ofBaseUnits(3, Inches); // placeholder, potentially different units
 
-    public static final int MOTOR_ID = 20;
+    public static final int MOTOR_ID = 30;
   }
 
   public static class RobotConstants {
