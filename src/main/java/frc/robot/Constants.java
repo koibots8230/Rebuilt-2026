bf5d2fd7a9e7c7473050c386d3d3bc69849f04e8
KoiBots Laptop <nobody@example.com>
2026-02-12 16:47:53 -0500

    changed p in PIDGains

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -145,7 +145,7 @@ public class Constants {
     public static final FeedforwardGains CLIMBER_FF =
         new FeedforwardGains.Builder().kv(60).ks(0.0).build(); // placeholder
     public static final PIDGains CLIMBER_PID =
-        new PIDGains.Builder().kp(6.0).build(); // placeholder
+        new PIDGains.Builder().kp(12.0).build(); // placeholder
 
     public static final Current CURRENT_LIMIT = Current.ofBaseUnits(60, Amps);
     public static final AngularVelocity ROTATIONS_PER_MINUTE = RPM.of(3000);
