225345bfbb00ba65eea2478f5a948824a6fd87bb
ConradSkelly <conradskelly@gmail.com>
2026-02-15 15:10:31 -0500

    stuff

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -103,6 +103,7 @@ public class Swerve extends SubsystemBase {
     odometryPose =
         odometry.update(
             isBlue ? gyroAngle : gyroAngle.minus(new Rotation2d(Math.PI)), modulePosition());
+
   }
 
   @Override
