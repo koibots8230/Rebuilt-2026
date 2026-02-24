b36cc6ae01183164be07935a29964571a61997ab
Jackson Shartzer <jacksonshartzer@gmail.com>
2026-02-24 16:45:12 -0500

    Added some game logic. Brain fried.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,8 @@ import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.LED;
+import frc.robot.subsystems.LED.LEDMode;
 
 @Logged
 public class Robot extends TimedRobot {
@@ -45,6 +47,7 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       CommandScheduler.getInstance().schedule(m_autonomousCommand);
     }
+
   }
 
   @Override
